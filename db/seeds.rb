008749bf2f1c3f0a2172924644513aed776fc9e7
ihollander <ianholla@gmail.com>
2021-07-19 06:30:56 -0400

    Added solution

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,15 @@
 # Add a console message so we can see output when the seed file runs
+puts "Seeding games..."
+
+# run a loop 50 times
+50.times do
+  # create a game with random data
+  Game.create(
+    title: Faker::Game.title,
+    genre: Faker::Game.genre,
+    platform: Faker::Game.platform,
+    price: rand(0..60) # random number between 0 and 60
+  )
+end
+
+puts "Done seeding!"
